3a95068fa96bfd7b313d321280f783d5f407e670
Paul Forsans <polfor33@gmail.com>
2023-12-26 01:09:19 +0100

    added mongoose support

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,11 +8,17 @@ export default defineNuxtConfig({
 	alias: {
 		"@": resolve(__dirname, "/")
 	},
-	postcss: {
+	postcss: { 
 		plugins: {
 			tailwindcss: {},
 			autoprefixer: {}
 		}
+	},
+	runtimeConfig: {
+		mongoUrl: `mongodb://${process.env.MONGO_ROOT_USERNAME}:${process.env.MONGO_ROOT_PASSWORD}@${process.env.MONGO_HOSTNAME}:${process.env.MONGO_PORT}/toptracker`
+	},
+	nitro: {
+		plugins: ["~/server/index.ts"]
 	},
 	css: ["~/assets/css/main.css"]
 });
