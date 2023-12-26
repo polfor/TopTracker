29e70a50a01a87b0ea0b086e364b24caf8c44f50
Paul Forsans <polfor33@gmail.com>
2023-12-26 01:09:31 +0100

    first model: room

---
+++ server/models/room.ts
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const roomSchema = new Schema({
+    name: String,
+    id: String,
+    users: [
+        {
+            spotifyId: String,
+            name: String,
+            image: {
+                url: String,
+                height: Number,
+                width: Number
+            },
+            topTracks: {
+                name: String,
+                artist: String,
+                images: [
+                    {
+                        url: String,
+                        height: Number,
+                        width: Number
+                    }
+                ]
+            }
+        }
+    ]
+});
