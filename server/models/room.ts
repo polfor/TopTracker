05bcb5ee8d9d34ab3d19daf7abe4f425b57172ac
Paul Forsans <polfor33@gmail.com>
2023-12-26 01:23:57 +0100

    added model export

--- server/models/room.ts
+++ server/models/room.ts
@@ -2,28 +2,30 @@ import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const roomSchema = new Schema({
-    name: String,
-    id: String,
-    users: [
-        {
-            spotifyId: String,
-            name: String,
-            image: {
-                url: String,
-                height: Number,
-                width: Number
-            },
-            topTracks: {
-                name: String,
-                artist: String,
-                images: [
-                    {
-                        url: String,
-                        height: Number,
-                        width: Number
-                    }
-                ]
-            }
-        }
-    ]
+	name: String,
+	id: String,
+	users: [
+		{
+			spotifyId: String,
+			name: String,
+			image: {
+				url: String,
+				height: Number,
+				width: Number
+			},
+			topTracks: {
+				name: String,
+				artist: String,
+				images: [
+					{
+						url: String,
+						height: Number,
+						width: Number
+					}
+				]
+			}
+		}
+	]
 });
+
+export default mongoose.model("Room", roomSchema);
