a6ff6fe462da61194db3ea0f3cf4cf7d8fc6ca7a
Paul Forsans <polfor33@gmail.com>
2024-01-04 16:32:26 +0100

    first component

---
+++ App.vue
@@ -0,0 +1,7 @@
+<template>
+	<div>
+		<LoginComponent />
+	</div>
+</template>
+
+<script setup></script>
---
+++ components/LoginComponent.vue
@@ -0,0 +1,16 @@
+<template>
+	<div class="flex flex-col items-center justify-center py-16">
+		<button
+			class="bg-green-500 hover:bg-green-600 text-white font-medium py-2 px-4 rounded-full transition-colors">
+			Login
+		</button>
+	</div>
+</template>
+
+<script lang="ts" setup>
+	const config = useRuntimeConfig();
+
+	const clientId = config.public.spotifyClientId;
+
+	console.log(clientId);
+</script>
