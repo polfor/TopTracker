44a831d79abb011c5f332ee927648e3776f930e7
Paul Forsans <polfor33@gmail.com>
2024-01-04 16:32:18 +0100

    ran mongodb_init through linter

--- docker/mongodb/scripts/mongodb_init.js
+++ docker/mongodb/scripts/mongodb_init.js
@@ -1,12 +1,14 @@
-const userName = process.env['MONGO_USER'];
-const password = process.env['MONGO_PASSWORD'];
+const userName = process.env["MONGO_USER"];
+const password = process.env["MONGO_PASSWORD"];
 db.createUser({
-    user: userName,
-    pwd: password,
-    roles: [{
-        role: 'readWrite',
-        db: 'toptracker'
-    }]
+	user: userName,
+	pwd: password,
+	roles: [
+		{
+			role: "readWrite",
+			db: "toptracker"
+		}
+	]
 });
 
-db.createCollection('temp');
+db.createCollection("temp");
